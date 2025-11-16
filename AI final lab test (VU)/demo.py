6420b97e853a735e25e408bd899ef0971fab878d
tamimystic <tamimhossainforcoding01@gmail.com>
2025-11-17 03:18:01 +0600

    implement KNN (classification model) in in small csv data

---
+++ AI final lab test (VU)/demo.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+import numpy as np 
+
+ 
+
+data=pd.read_csv('C:\Inception BD\Data-Science-with-Generative-AI\AI final lab test (VU)\sports_data.csv')  
+
+print(data.shape) 
+
+data.describe() 
+
+data['height'] 
+
+data['weight'] 
