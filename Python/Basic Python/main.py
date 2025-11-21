fb1958a84b000f4c0ef084ca0fddeffd46a84d50
tamimystic <tamimhossainforcoding01@gmail.com>
2025-11-21 19:13:30 +0600

    folder name change

--- Python/Basic Python/main.py
+++
@@ -1,9 +0,0 @@
-
-from fastapi import FastAPI
-app=FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-if __name__=="__main__":
-    import uvicorn
-    uvicorn.run("main:app",host="127.0.0.1",port=8000,reload=True)
